03203fad1a1d88578b7fab975e635d172c9704a5
Ian-c-cameron <ian.c.cam@gmail.com>
2020-10-04 22:30:33 +0000

    created gameboard class to store the status of the board, add ships to the board, and determine the results of an attack

---
+++ gameBoard.js
@@ -0,0 +1,102 @@
+class Gameboard {
+  /**
+   * constructor - sets the size of the game board
+   * @param {*} maxNum highest number on the Y axis
+   * @param {*} maxLetter Highest letter on the X axis
+   */
+  constructor(maxNum, maxLetter) {
+    this.maxNum = maxNum;
+    this.maxLetter = maxLetter;
+    //0=empty and untried, 1=empty and tried, 2=hit but not sunk, 3=sunk
+    for (let x = 1; x <= maxNum; x++){
+      for (let char = a; char <= maxLetter; char = String.fromCharCode(char.charCodeAt(0) + 1)) {
+        this[char + x] = 0;
+      }
+    }
+  }
+
+  /**
+   * markShip - marks that a position on the game board is occupied 
+   * @param {*} position A position occupied by the ship
+   * @param {*} ship The ship object that occupies the space
+   * @param {*} direction true=horizontal, false vertical
+   */
+  markShip(position, ship, direction) {
+    let index = position[0] + position[1];
+    if (direction){
+      if (position[1] + (ship.length -1) > this.maxNum){
+        return false;
+      }
+      for (let x = position[1]; x < position[1] + ship.length; x++) {
+        this[position[0] + x] = ship;
+        ship.occupies(position[0] + x);
+      }
+      return true;
+    }
+    const lastLetter = String.fromCharCode(position[0].charCodeAt(0) + (ship.length-1));
+    if (lastLetter > this.maxLetter) {
+      return false;
+    }
+    for (let char = position[0]; char < lastLetter; char = String.fromCharCode(char.charCodeAt(0) + 1)) {
+      this[char + position[1]] = ship;
+      ship.occupies(char + position[1]);
+    }
+    return true;
+  }
+
+  /**
+   * attack - an enemy attacks a position on the game board
+   * @param {*} position  - the position that is targeted
+   */
+  attack(position) {
+    let index = position[0] + position[1];
+    if (!this[index]){
+      this[index] = 1;
+      return 'You Missed!';
+    }
+    if (typeof this[index] === 'number') {
+      return 'Already tried that one!';
+    }
+
+    //the attack is a hit!
+    let sunk = this[index].hit();
+    if (sunk) {
+      for (const hit of sunk) {
+        this[hit] = 3;
+        return 'Ship Sunk!'
+      }
+    }
+    this[index] = 2
+    return 'Hit!';
+  }
+}
+
+
+
+validateCoord([letter, num]) {
+  //invalidate non strings and too long or short
+  if (typeof letter !== 'string' || typeof num !== 'number') {
+    return false;
+  }
+  if (letter.length !== 1) {
+    console.log('invalid letter: more than one char');
+    return false;
+  }
+  if (!Number.isInteger(num)) {
+    console.log('invalid number: not an Int');
+    return false;
+  }
+
+  //invalidate letters outside the valid range
+  if (letter < 'a' || letter > this.maxLetter) {
+    console.log('invalid letter: out of range');
+    return false;
+  }
+
+  //invalidate numbers outside the valid range
+  if (num < 1 || num > this.maxNum) {
+    console.log('invalid number: out of range');
+    return false;
+  }
+  return true;
+}
